2b009ea8cc93971631cab485432eb92c8e3735b4
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-12-01 14:58:36 -0500

    Add GitHub organizations

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 docker build -t jenkins:localtest .
 # https://stackoverflow.com/a/75015304/13461208
 # Preserve newline characters in multiline environment variables by calling cat on the file and passing the output to the environment variable
-docker run --rm --volume /var/jenkins_home -p 8080:8080 --env-file .env -e JENKINS_CREDENTIAL_JENKINS_GITHUB_ANDREWJDAWES_PRIVATE_KEY="$(cat converted-jenkins-github-andrewjdawes.pem)" jenkins:localtest
+docker run --rm --volume /var/jenkins_home -p 8080:8080 --env-file .env -e JENKINS_CREDENTIAL_JENKINS_GITHUB_ANDREWJDAWES_PRIVATE_KEY="$(cat converted-jenkins-github-andrewjdawes.pem)" -e JENKINS_CREDENTIAL_JENKINS_GITHUB_CODEKAIZEN_PRIVATE_KEY="$(cat converted-jenkins-github-codekaizen.pem)" jenkins:localtest
