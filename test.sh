d685b782ba529af9c2e75db6c40633d65db62278
Missie Dawes <missiedawes@gmail.com>
2024-12-14 15:29:27 -0500

    add missie app

--- test.sh
+++ test.sh
@@ -2,4 +2,9 @@
 # Preserve newline characters in multiline environment variables by calling cat on the file and passing the output to the environment variable
 # docker run --rm --volume /var/jenkins_home -p 8080:8080 --env-file .env -e JENKINS_CREDENTIAL_GITHUB_APP_USER_ANDREWJDAWES_SSH_KEY_PRIVATE="$(cat converted-github-app-user-andrewjdawes.pem)" -e JENKINS_CREDENTIAL_GITHUB_APP_ORGANIZATION_CODEKAIZEN_SSH_KEY_PRIVATE="$(cat converted-github-app-organization-codekaizen.pem)" ghcr.io/andrewjdawes/home-lab-jenkins:latest
 docker build -t home-lab-jenkins:test .
-docker run --name jenkins --rm --volume /var/jenkins_data -p 8080:8080 --env-file .env -e JENKINS_CREDENTIAL_JENKINS_CONTROLLER_TO_AGENTS_PRIVATE_KEY="$(cat jenkins-controller-to-agents-private-key.pem)" -e JENKINS_CREDENTIAL_GITHUB_APP_USER_ANDREWJDAWES_SSH_KEY_PRIVATE="$(cat converted-github-app-user-andrewjdawes.pem)" -e JENKINS_CREDENTIAL_GITHUB_APP_ORGANIZATION_CODEKAIZEN_SSH_KEY_PRIVATE="$(cat converted-github-app-organization-codekaizen.pem)" home-lab-jenkins:test
+docker run --name jenkins --rm --volume /var/jenkins_data -p 8080:8080 --env-file .env \
+    -e JENKINS_CREDENTIAL_JENKINS_CONTROLLER_TO_AGENTS_PRIVATE_KEY="$(cat jenkins-controller-to-agents-private-key.pem)" \
+    -e JENKINS_CREDENTIAL_GITHUB_APP_USER_ANDREWJDAWES_SSH_KEY_PRIVATE="$(cat converted-github-app-user-andrewjdawes.pem)" \
+    -e JENKINS_CREDENTIAL_GITHUB_APP_USER_MISSIEDAWES_SSH_KEY_PRIVATE="$(cat converted-github-app-user-missiedawes.pem)" \
+    -e JENKINS_CREDENTIAL_GITHUB_APP_ORGANIZATION_CODEKAIZEN_SSH_KEY_PRIVATE="$(cat converted-github-app-organization-codekaizen.pem)" \
+    home-lab-jenkins:test
