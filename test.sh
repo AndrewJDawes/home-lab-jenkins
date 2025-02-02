aa337f6c7778ac87c3e0308951664017096ce055
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-02-02 15:13:37 -0500

    Working conjur connection

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@
 docker build -t home-lab-jenkins:test .
 # If network minio is not created, create it
 docker network create minio_reverse_proxy || true
+docker network create conjur_external || true
 docker run --name jenkins --rm --volume /var/jenkins_data -p 8073:8080 --env-file .env \
     -e JENKINS_CREDENTIAL_JENKINS_CONTROLLER_TO_AGENTS_PRIVATE_KEY="$(cat jenkins-controller-to-agents-private-key.pem)" \
     -e JENKINS_CREDENTIAL_GITHUB_APP_USER_ANDREWJDAWES_SSH_KEY_PRIVATE="$(cat converted-github-app-user-andrewjdawes.pem)" \
@@ -12,4 +13,5 @@ docker run --name jenkins --rm --volume /var/jenkins_data -p 8073:8080 --env-fil
     -e JENKINS_CREDENTIAL_GITHUB_APP_ORGANIZATION_CODEKAIZEN_SSH_KEY_PRIVATE="$(cat converted-github-app-organization-codekaizen.pem)" \
     -e JENKINS_CREDENTIAL_APPLICATION_DEPLOYMENT_PREVIOUSLY_AUTHORIZED_SSH_KEY_PRIVATE="$(cat application-deployment-previously-authorized-private-key.pem)" \
     --network minio_reverse_proxy \
+    --network conjur_external \
     home-lab-jenkins:test
